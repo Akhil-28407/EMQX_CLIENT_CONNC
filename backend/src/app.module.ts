3ca20a6b6c0b508f1d8b8e83b9cd4127be873e8e
Akhil-28407 <ibascakhil@gmail.com>
2026-03-26 12:16:31 +0530

    vercel.json is added for vercel deploy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { MqttModule } from './mqtt/mqtt.module';
 
 @Module({
   imports: [MqttModule],
+  controllers: [AppController],
 })
 export class AppModule {}
